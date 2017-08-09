47c58a6ed33d0d4edd0bd265153ec269e323723b
Domenic Corso <s3545195@student.rmit.edu.au>
2017-08-09 22:10:16 +1000

    Added restroom_input partial

---
+++ resources/views/partials/restroom_input.blade.php
@@ -0,0 +1,21 @@
+<div class="form-group">
+    <label for="rr_name">Name</label>
+    <input type="text" name="rr_name" class="form-control" id="rr_name" />
+</div>
+<div class="form-group">
+    <label for="rr_desc">Short Description</label>
+    <input type="text" name="rr_desc" class="form-control" id="rr_desc" />
+</div>
+<div class="form-group">
+    <label for="rr_lat">Latitude</label>
+    <input type="text" name="rr_lat" class="form-control" id="rr_desc" disabled />
+</div>
+<div class="form-group">
+    <label for="rr_lng">Longitude</label>
+    <input type="text" name="rr_lng" class="form-control" id="rr_lng" disabled />
+</div>
+<div class="form-group">
+    <label for="rr_floor">Floor<label>
+    <input type="text" name="rr_floor" class="form-control" id="rr_floor" />
+</div>
+<button type="submit">Save Changes</button>
