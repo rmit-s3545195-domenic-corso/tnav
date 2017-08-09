fa5f0a454bcd2cc83dba460fb9973d5210098b7b
Domenic Corso <s3545195@student.rmit.edu.au>
2017-08-09 21:48:39 +1000

    Added styles + scripts yield to layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,14 @@
         <meta charset="utf-8" />
         <title>@yield('title') | TNav</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ url('/css/app_layout.css') }}" /> 
+        <link rel="stylesheet" href="{{ url('/css/app_layout.css') }}" />
+        @yield('styles') 
     </head>
     <body>
         <div id="header"></div>
         <div id="content">
             @yield('content')
         </div>
+        @yield('scripts')
     </body>
 </html>
