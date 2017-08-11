2aed26eea4cca283d7808673ba2cd7dc0e5f3cee
Varsha Singh <s3544206@student.rmit.edu.au>
2017-08-11 20:56:08 +1000

    Geolocation Files
    Created tnav.js and updates tna.location file

--- public/js/tnav.location.js
+++ public/js/tnav.location.js
@@ -8,21 +8,15 @@ If the function succeeds
 function success(position)
 {
   /*
-  Gets longitude and latitude
+  Gets co-ordinates
   */
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-
-  /*
-  Returns googles API LatLng
-  */
-  var googleCord = new google.maps.LatLng(lat, long);
+  var coords = {
+    lat: position.coords.latitude,
+    long: position.coords.longitude
+  };
 }
 
-/*
-When the function fails
-*/
+
 function failure()
 {
-  infoWindow.setContent('Error: Geolocation Service Failed');
 }
