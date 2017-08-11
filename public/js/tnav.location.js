9f011aefc1f12a1dac9b8433b05563aa9e5d0f0c
Varsha Singh <s3544206@student.rmit.edu.au>
2017-08-11 22:34:16 +1000

    getPosition made
    Removed success and failure functions as they are not to be defined here.

--- public/js/tnav.location.js
+++ public/js/tnav.location.js
@@ -1,22 +1,10 @@
-geoloc = navigator.geolocation;
+let geoloc = navigator.geolocation;
 
-tnav.location = geoloc.getCurrentPosition(success, failure);
+tnav.location.getPosition = geloc.getCurrentPosition(success, failure); {
 
-/*
-If the function succeeds
-*/
-function success(position)
-{
-  /*
-  Gets co-ordinates
-  */
-    var coords = {
-      lat: position.coords.latitude,
-      long: position.coords.longitude
-  };
-}
-
-
-function failure()
-{
+/* Gets co-ordinates */
+      var coords = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+    };
 }
