c5c94c012383e0e8cc2a47ad1c4883baeee2acb0
rmit-s3545195-domenic-corso <s3545195@student.rmit.edu.au>
2017-08-14 15:58:27 +1000

    Geolocation (#2)
    * Geolocation
    
    Initial js code based off tutorials - Should return what we want for the API
    
    * Geolocation Files
    
    Created tnav.js and updates tna.location file
    
    * tnav.location object
    
    Placed function withing tnav.location object
    
    * getPosition made
    
    Removed success and failure functions as they are not to be defined here.
    
    * Final Geolocation fix
    
    Final version of file AS FAR AS USER STORY 20 IS CONCERNED.

---
+++ public/js/tnav.location.js
@@ -0,0 +1,6 @@
+let tnav.location {};
+
+tnav.location.getPosition = function(success, failure) {
+    /*get's location, success and failure commands determined by external code*/
+    navigator.geolocation.getCurrentPosition(success,failure);
+};
