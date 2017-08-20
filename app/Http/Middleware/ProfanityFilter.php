ed4b66c0395213ced5e7a9ddbd6949d1febc880b
Varsha Singh <s3544206@student.rmit.edu.au>
2017-08-20 13:38:00 +1000

    fixed value being replaced

--- app/Http/Middleware/ProfanityFilter.php
+++ app/Http/Middleware/ProfanityFilter.php
@@ -20,11 +20,11 @@ class ProfanityFilter
         $profanitylist = file('/var/www/html/tnav/storage/profanitylist.txt', FILE_SKIP_EMPTY_LINES);
         $replaceProfanity = "****";
 
-        foreach ($input as $value)
+        foreach($input as $value)
         {
-            if (in_array($value, $profanitylist)
+            if(in_array($value, $profanitylist)
             {
-            $fixedprofanity = str_ireplace($profanitylist, $replaceProfanity, $value);
+            $fixedprofanity = str_ireplace($value, $replaceProfanity, $value);
             return $fixedprofanity;
             }
         }
