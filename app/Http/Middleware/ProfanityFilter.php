1a4264b3fdfeab243ccac2fee60e1d990d51dc20
rmit-s3544206-varsha-singh <s3544206@student.rmit.edu.au>
2017-09-07 23:13:14 +1000

    Simplified if statements into foreach, simplified profanitylist path, made censorship dynamic

--- app/Http/Middleware/ProfanityFilter.php
+++ app/Http/Middleware/ProfanityFilter.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Storage;
 
 class ProfanityFilter
 {
@@ -16,31 +17,30 @@ class ProfanityFilter
     *
     */
     public function handle($request, Closure $next) {
-
-        $rr_name = $request->rr_name;
-        $rr_desc = $request->rr_desc;
-        $rr_added_by = $request->rr_added_by;
-        $rr_floor = $request->rr_floor;
-        $profanitylist = file('/var/www/html/tnav/storage/profanitylist.txt', FILE_SKIP_EMPTY_LINES);
-	
-	foreach($profanitylist as $profanity) {
-        if($rr_name = $profanity) {
-            $request->merge(['rr_name' => "*****"]);
-        }
-
-        if($rr_desc = $profanity) {
-            $request->merge(['rr_desc' => "*****"]);
-        }
-
-        if($rr_added_by = $profanity) {
-            $request->merge(['rr_added_by' => "*****"]);
-        }
-
-        if($rr_floor = $profanity) {
-            $request->merge(['rr_floor' => "*****"]);
-        }
+      	if (!count($request->all())) {
+	    return $next($request);
 	}
 
+        $profanityList = file(storage_path('profanitylist.txt'), FILE_SKIP_EMPTY_LINES);
+	$profanityList = array_map("trim", $profanityList);
+
+	foreach($request->except('_token') as $k => $v) {
+            $modifiedValue = $v;
+	    foreach($profanityList as $p) {
+		if(strpos(strtolower($v), $p) !== false) {
+	          /* Replace characters with stars */
+                  $modifiedValue = str_replace($p, self::stars(strlen($p)), $modifiedValue);
+		}
+	    }
+           $request->merge([$k => $modifiedValue]);
+	}
         return $next($request);
+    }
+
+    private function stars($amount) {
+        $stars = "";
+        for ($i = 0; $i < $amount; $i++) {
+            $stars .= "*";
+        } return $stars;
     }
 }
