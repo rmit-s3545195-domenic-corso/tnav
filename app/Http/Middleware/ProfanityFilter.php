74f7deae627473ba525f707f41868c75ccc6fa99
Varsha Singh <s3544206@student.rmit.edu.au>
2017-09-08 10:08:32 +1000

    Meaningful comment

--- app/Http/Middleware/ProfanityFilter.php
+++ app/Http/Middleware/ProfanityFilter.php
@@ -17,27 +17,33 @@ class ProfanityFilter
     *
     */
     public function handle($request, Closure $next) {
-      	if (!count($request->all())) {
-	    return $next($request);
-	}
+        /* If there is no input in the request then don't run */
+        if (!count($request->all())) {
+	         return $next($request);
+	      }
 
         $profanityList = file(storage_path('profanitylist.txt'), FILE_SKIP_EMPTY_LINES);
-	$profanityList = array_map("trim", $profanityList);
+	      $profanityList = array_map("trim", $profanityList);
 
-	foreach($request->except('_token') as $k => $v) {
-            if (!is_string($v)) { continue; }
-            $modifiedValue = $v;
-	    foreach($profanityList as $p) {
-		if(strpos(strtolower($v), $p) !== false) {
-	          /* Replace characters with stars */
-                  $modifiedValue = str_replace($p, self::stars(strlen($p)), $modifiedValue);
-		}
-	    }
+        /* Goes  through each request input first*/
+	      foreach($request->except('_token') as $k => $v) {
+          if (!is_string($v)) {
+            continue;
+          }
+          $modifiedValue = $v;
+          /* Goes through each profanity */
+	        foreach($profanityList as $p) {
+		          if(strpos(strtolower($v), $p) !== false) {
+	               /* Replace characters with stars */
+                $modifiedValue = str_replace($p, self::stars(strlen($p)), $modifiedValue);
+		          }
+	         }
            $request->merge([$k => $modifiedValue]);
-	}
+	      }
         return $next($request);
-    }
+      }
 
+    /* Function to convert characters into stars */
     private function stars($amount) {
         $stars = "";
         for ($i = 0; $i < $amount; $i++) {
